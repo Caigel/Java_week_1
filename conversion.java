2e9c541c6fdd3950050076bef1e6339455ce6e71
Caigel <97202202+Caigel@users.noreply.github.com>
2025-08-28 18:42:39 -0400

    Add files via upload

---
+++ conversion.java
@@ -0,0 +1,14 @@
+// Conversion class
+public class Conversion {
+
+    /** Convert from feet to meters */
+    public static double footToMeter(double foot) {
+        return 0.305 * foot;
+    }
+
+    /** Convert from meters to feet */
+    public static double meterToFoot(double meter) {
+        return 3.279 * meter;
+    }
+}
+
